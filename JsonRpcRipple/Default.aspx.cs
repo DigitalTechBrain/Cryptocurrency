162c5e3c4641a8f2e1f99b3d49d7dddfb3e4b12c
Jitender kumar <letsexplorebrain@gmail.com>
2018-03-09 15:47:52 +0530

    Error in Jsonrpc

--- JsonRpcRipple/Default.aspx.cs
+++ JsonRpcRipple/Default.aspx.cs
@@ -1,17 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace JsonRpcRipple
 {
     public partial class Default : System.Web.UI.Page
     {
+        public JObject InvokeMethod(string a_sMethod, params object[] a_params)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(@"https://s.altnet.rippletest.net:51234");
+            // webRequest.Credentials = Credentials;
+
+            webRequest.ContentType = "application/json-rpc";
+            webRequest.Method = "POST";
+
+            JObject joe = new JObject();
+            joe["jsonrpc"] = "1.0";
+            joe["id"] = "1";
+            joe["command"] = a_sMethod;
+            joe["tx_hash"] = "E08D6E9754025BA2534A78707605E0601F03ACE063687A0CA1BDDACFCD1698C7";
+            joe["ledger_index"] = "348734";
+
+            if (a_params != null)
+            {
+                if (a_params.Length > 0)
+                {
+                    JArray props = new JArray();
+                    foreach (var p in a_params)
+                    {
+                        props.Add(p);
+                    }
+                    joe.Add(new JProperty("params", props));
+                }
+            }
+
+            string s = JsonConvert.SerializeObject(joe);
+            // serialize json for the request
+            byte[] byteArray = Encoding.UTF8.GetBytes(s);
+            webRequest.ContentLength = byteArray.Length;
+
+            try
+            {
+                using (Stream dataStream = webRequest.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                }
+            }
+            catch (WebException we)
+            {
+                //inner exception is socket
+                //{"A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond 23.23.246.5:8332"}
+                throw we;
+            }
+            WebResponse webResponse = null;
+            try
+            {
+                using (webResponse = webRequest.GetResponse())
+                {
+                    using (Stream str = webResponse.GetResponseStream())
+                    {
+                        using (StreamReader sr = new StreamReader(str))
+                        {
+                            return JsonConvert.DeserializeObject<JObject>(sr.ReadToEnd());
+                        }
+                    }
+                }
+            }
+            catch (WebException webex)
+            {
+
+                using (Stream str = webex.Response.GetResponseStream())
+                {
+                    using (StreamReader sr = new StreamReader(str))
+                    {
+                        var tempRet = JsonConvert.DeserializeObject<JObject>(sr.ReadToEnd());
+                        return tempRet;
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var ret = InvokeMethod("transaction_entry", "index");
         }
     }
 }
